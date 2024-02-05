6590dd0b121b3e2bf10c55f2781b0bfe4dceddf4
Srinivas Pokala <Pokala.Srinivas@ibm.com>
2024-02-05 19:50:43 +0100

    Implemented s390xutil package

---
+++ s390x/s390xutil/hack.h
@@ -0,0 +1,59 @@
+// Copyright 2021 The Go Authors.  All rights reserved.
+// // Use of this source code is governed by a BSD-style
+// // license that can be found in the LICENSE file.
+// //
+// // This file requires gcc and binutils with -mcpu=power10 support.
+// // s390xutil runs a series of commands like:
+// //   go run map.go -fmt=asm ../pp64.csv > asm.S
+// //   powerpc64le-linux-gnu-gcc -c asm.S -mcpu=power10 -mbig
+// //   powerpc64le-linux-gnu-objdump -d asm.o
+// // to create the file decode_generated.txt used to verify the disassembler.
+// //
+// // Note, the golang disassembler is not expected to support every extended
+// // mnemonic, but it should support those which frequently show up in object
+// // files compiled by the golang toolchain.
+
+#define R1 8
+#define R2 0
+#define R3 0
+
+#define X2 2
+
+#define L1 4
+#define L2 4
+
+#define B1 2
+#define B2 1
+#define B3 6
+#define B4 8
+
+#define D1 6
+#define D2 11
+#define D3 182
+#define D4 205
+
+#define V1 18
+#define V2 3
+#define V3 5
+#define V4 8
+
+
+
+#define I 124
+#define I1 12
+#define I2 8
+#define I3 9
+#define I4 105
+#define I5 18
+
+
+#define RI2 -100
+#define RI3 294
+#define RI4 -168
+
+
+#define M1 1
+#define M3 3
+#define M4 2
+#define M5 3
+#define M6 2
