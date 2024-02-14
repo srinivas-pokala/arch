19267503e21680e1714835b5394174c6ac3c61eb
Srinivas Pokala <Pokala.Srinivas@ibm.com>
2024-02-14 19:35:11 +0100

    Changes for decode_test.go for testsuite

--- s390x/s390xasm/decode_test.go
+++ s390x/s390xasm/decode_test.go
@@ -5,13 +5,11 @@
 package s390xasm
 
 import (
-	//"encoding/binary"
 	"encoding/hex"
 	"io/ioutil"
 	"path"
 	"strings"
 	"testing"
-	"fmt"
 )
 
 func TestDecode(t *testing.T) {
@@ -49,14 +47,11 @@ func decode(data []byte, t *testing.T, filename string) {
 		all = strings.Replace(all, "\t\t", "\t", -1)
 	}
 	for _, line := range strings.Split(all, "\n") {
-		fmt.Printf("Srinivas: Before:%s\n",line)
 		line = strings.TrimSpace(line)
-		fmt.Printf("Srinivas: After:%s\n",line)
 		if line == "" || strings.HasPrefix(line, "#") {
 			continue
 		}
 		f := strings.SplitN(line, "\t", 3)
-		fmt.Printf("Srinivas: f list: %s\n", f)
 		i := strings.Index(f[0], "|")
 		if i < 0 {
 			t.Errorf("%s: parsing %q: missing | separator", filename, f[0])
@@ -66,14 +61,12 @@ func decode(data []byte, t *testing.T, filename string) {
 			t.Errorf("%s: parsing %q: misaligned | separator", filename, f[0])
 		}
 		size := i / 2
-		fmt.Printf("Srinivas: size: %v\n", size)
 		code, err := hex.DecodeString(f[0][:i] + f[0][i+1:])
 		if err != nil {
 			t.Errorf("%s: parsing %q: %v", filename, f[0], err)
 			continue
 		}
 		syntax, asm := f[1], f[2]
-		fmt.Printf("Srinivas: syntax:%s asm:%s\n", syntax, asm)
 		inst, err := Decode(code)
 		var out string
 		if err != nil {
@@ -83,14 +76,14 @@ func decode(data []byte, t *testing.T, filename string) {
 			case "gnu":
 				out = GNUSyntax(inst, pc)
 			/*case "plan9":
-				pc := pc
-				// Hack: Setting PC to 0 effectively transforms the PC relative address
-				// of CALL (bl) into an absolute address when decoding in GoSyntax. This
-				// simplifies the testing of symbol lookups via symlookup above.
-				if inst.Op == BL {
-					pc = 0
-				}
-				out = GoSyntax(inst, pc, symlookup)*/
+			pc := pc
+			// Hack: Setting PC to 0 effectively transforms the PC relative address
+			// of CALL (bl) into an absolute address when decoding in GoSyntax. This
+			// simplifies the testing of symbol lookups via symlookup above.
+			if inst.Op == BL {
+				pc = 0
+			}
+			out = GoSyntax(inst, pc, symlookup)*/
 			default:
 				t.Errorf("unknown syntax %q", syntax)
 				continue
