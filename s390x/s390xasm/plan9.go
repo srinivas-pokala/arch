877340d8a2e7d151e8658b6f52c3453ee749c02e
Srinivas Pokala <Pokala.Srinivas@ibm.com>
2024-08-23 07:04:51 +0200

    Build fixes

--- s390x/s390xasm/plan9.go
+++ s390x/s390xasm/plan9.go
@@ -574,7 +574,7 @@ func GoSyntax(inst Inst, pc uint64, symname func(uint64) (string, uint64)) strin
 		val := mask & 0x7
 		switch inst.Op {
 		case VA, VS, VACC:
-			if val && val < 5 {
+			if !!val && val < 5 {
 				op = op + vectorSize[val]
 				args[0], args[1], args[2] = args[1], args[2], args[0]
 				args = args[:3]
@@ -582,7 +582,7 @@ func GoSyntax(inst Inst, pc uint64, symname func(uint64) (string, uint64)) strin
 				return fmt.Sprintf("Specefication exception is recognized for %q with mask value: %v \n", op, mask)
 			}
 		case VAVG, VAVGL:
-			if val && val < 4 {
+			if !!val && val < 4 {
 				op = op + vectorSize[val]
 				args[0], args[1], args[2] = args[1], args[2], args[0]
 				args = args[:3]
